446f4ddfbe1595f180e5f18c3a6c2ed9ba36f55b
Yoni Lerner <yonilern@gmail.com>
2022-02-10 13:01:33 -0800

    Added challenger mode

--- sim.js
+++ sim.js
@@ -1,14 +1,15 @@
 const allowIllegalTeams = false;
 const functionalProgressBar = true; // Turn this off to make code a little faster (experimentally 18 mins => 17 mins) but break the progress bar
+// Turn this on to treat the first team as the "challenger" and only have it battle other teams instead of having every pair battle
+// Useful for testing a team concept against a collection of common teams
+const challengerMode = true;
 
 
 
-
-import pokemonShowdown from 'pokemon-showdown';
-const {BattleStream, Teams, TeamValidator} = pokemonShowdown;
-import fs from 'fs';
-import cliProgress from 'cli-progress';
-import chalk from 'chalk';
+const {BattleStream, Teams, TeamValidator} = require('pokemon-showdown');
+const fs = require('fs');
+const cliProgress = require('cli-progress');
+const chalk = require('chalk');
 
 // Parse args
 let file, trials;
@@ -87,20 +88,32 @@ async function simBattle(team1, team2, doPrint) {
 }
 
 const teamNames = Object.keys(teams);
-let winMatrix = Array(teamNames.length).fill().map(()=>Array(teamNames.length).fill(0));
 const promises = [];
+let winMatrix = Array(teamNames.length).fill().map(()=>Array(teamNames.length).fill(0));
+let totalTrials;
 
-console.log(`Beginning tournament of ${teamNames.length} teams with each pair facing off ${trials} times`);
-console.log(`Teams:`);
-teamNames.forEach((x, i) => console.log(`  ${i+1}\t${x}`));
+if (challengerMode) {
+    console.log(`Beginning simulation of one team challenging all ${teamNames.length - 1} opponents ${trials} times each`);
+    console.log(`Challenger:\t${teamNames[0]}`)
+    console.log(`Teams:`);
+    teamNames.slice(1).forEach((x, i) => console.log(`  ${i+1}\t${x}`));
+    totalTrials = trials * (teamNames.length - 1);
+} else {
+    console.log(`Beginning tournament of ${teamNames.length} teams with each pair facing off ${trials} times`);
+    console.log(`Teams:`);
+    teamNames.forEach((x, i) => console.log(`  ${i+1}\t${x}`));
+    totalTrials = trials * (teamNames.length * (teamNames.length - 1) / 2);
+}
 
 const pbar = new cliProgress.SingleBar({format: '[{bar}] {percentage}% | Time: {duration_formatted} | ETA: {eta_formatted} | {value}/{total}'});
-pbar.start(trials * (teamNames.length * (teamNames.length - 1) / 2), 0);
+pbar.start(totalTrials, 0);
 
 promises.push((async () => {
     for (let trial = 0; trial < trials; trial++) {
         const promisesRound = []; 
-        for (let i = 0; i < teamNames.length; i++) {
+
+        const maxI = challengerMode ? 0 : teamNames.length - 1;
+        for (let i = 0; i <= maxI; i++) {
             for (let j = i + 1; j < teamNames.length; j++) {
                 let promise = simBattle(teams[teamNames[i]], teams[teamNames[j]])
                 promise.then((winner) => {
@@ -162,21 +175,38 @@ Promise.all(promises).then(() => {
 
     winMatrix = winMatrix.map((row) => row.map((x) => (x / trials * 100)))
 
-    let minVal = Math.max(2 * Math.min(...winMatrix.map((row, i) => row.filter((_, j) => j != i)).flat()) - 50, 0);
-    let maxVal = Math.min(2 * Math.max(...winMatrix.map((row, i) => row.filter((_, j) => j != i)).flat()) - 50, 100);
-
-    console.log("Winner in column, loser in row")
     const strWinMatrix = winMatrix.map((row) => row.map((x) => x.toFixed(1)))
     for (let i = 0; i < teamNames.length; i++) {
         strWinMatrix[i][i] = "-"
-        strWinMatrix[i].unshift(i + 1) // Add labels
+        strWinMatrix[i].unshift(i + (challengerMode ? 0 : 1)) // Add labels
     }
     strWinMatrix.unshift(Array.from(Array(teamNames.length + 1).keys())) // Add labels
     strWinMatrix[0][0] = "" // Fix corner
-    matprint(strWinMatrix, minVal, maxVal);
 
-    console.log("\n\nOverall winrates:\n")
+    if (challengerMode) {
+        let minVal = Math.min(
+            Math.min(...strWinMatrix.slice(2).map(row => row[1])),
+            Math.min(...strWinMatrix[1].slice(2))
+        )
+        let maxVal = Math.max(
+            Math.max(...strWinMatrix.slice(2).map(row => row[1])),
+            Math.max(...strWinMatrix[1].slice(2))
+        )
 
-    const avgRates = teamNames.map((name, i) => [name, winMatrix.reduce((sum, row, j) => i == j ? sum : sum + row[i], 0) / (winMatrix.length - 1)]).sort((a, b) => b[1]- a[1])
-    avgRates.forEach((pair, i) => console.log("   #" + (i+1) + "\t| " + getColor(pair[1], avgRates[avgRates.length-1][1], avgRates[0][1])(pair[1].toFixed(1)) + " | " + pair[0]))
+        console.log("Winrate for challenger:")
+        let cutMat = strWinMatrix.slice(2).map((row, i) => row.slice(0, 2).concat(teamNames[i+1]));
+        cutMat.unshift(["", "", ""]) // Dummy label
+        matprint(cutMat, minVal, maxVal);
+    } else {
+        let minVal = Math.max(2 * Math.min(...winMatrix.map((row, i) => row.filter((_, j) => j != i)).flat()) - 50, 0);
+        let maxVal = Math.min(2 * Math.max(...winMatrix.map((row, i) => row.filter((_, j) => j != i)).flat()) - 50, 100);
+
+        console.log("Winner in column, loser in row")
+        matprint(strWinMatrix, minVal, maxVal);
+
+        console.log("\n\nOverall winrates:\n")
+
+        const avgRates = teamNames.map((name, i) => [name, winMatrix.reduce((sum, row, j) => i == j ? sum : sum + row[i], 0) / (winMatrix.length - 1)]).sort((a, b) => b[1]- a[1])
+        avgRates.forEach((pair, i) => console.log("   #" + (i+1) + "\t| " + getColor(pair[1], avgRates[avgRates.length-1][1], avgRates[0][1])(pair[1].toFixed(1)) + " | " + pair[0]))
+    }
 });
