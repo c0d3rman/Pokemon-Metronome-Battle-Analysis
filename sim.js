db8236cd8fff75e548bef664c97ed8061b1d6f9a
Yoni Lerner <yonilern@gmail.com>
2022-02-12 13:30:38 -0800

    Added multiple challenger support
    Also added comments, changed command args, and cleaned things up a bit

--- sim.js
+++ sim.js
@@ -1,10 +1,3 @@
-const allowIllegalTeams = true;
-// Turn this on to treat the first team as the "challenger" and only have it battle other teams instead of having every pair battle
-// Useful for testing a team concept against a collection of common teams
-const challengerMode = true;
-
-
-
 const {Teams, TeamValidator} = require('./pokemon-showdown');
 const fs = require('fs');
 const cliProgress = require('cli-progress');
@@ -12,23 +5,41 @@ const chalk = require('chalk');
 const workerpool = require('workerpool');
 
 
-// Parse args
-let file, trials;
+
+// Parse command line args
+let opponentsFile, challengersFile, trials, isChallengerMode;
 try {
-    file = fs.readFileSync(process.argv[2]).toString().replace(/\r/g, "");
-    trials = parseInt(process.argv[3]);
-    if (process.argv.length != 4 || Number.isNaN(trials) || trials <= 0) {
-        throw new Error();
+    args = process.argv.slice(2)
+    if (args.length < 2 || args.length > 3) {
+        throw new Error("Too many or too few arguments");
     }
-} catch {
-    console.log("Example usage: node sim.js sample_teams.txt 100");
+
+    opponentsFile = fs.readFileSync(args[0]).toString().replace(/\r/g, "");
+    isChallengerMode = (args.length == 3);
+    if (isChallengerMode) {
+        trials = args[2];
+        challengersFile = fs.readFileSync(args[1]).toString().replace(/\r/g, "");
+    } else {
+        trials = args[1];
+    }
+
+    if (trials.match(/\D/) || parseInt(trials) <= 0) {
+        throw new Error("Invalid number of trials");
+    }
+    trials = parseInt(trials)
+} catch (e) {
+    console.log(e)
+    console.log(`Example usage:
+Round robin:\t\tnode sim.js meta_teams.txt 100
+Challenger mode:\tnode sim.js meta_teams.txt sample_teams.txt 10
+`);
     process.exit();
 }
 
-const validator = new TeamValidator('gen8metronomebattle');
-
 // Load all teams
-const teams = [...file
+const validator = new TeamValidator('gen8metronomebattle');
+function loadTeams(file) {
+    return [...file
     .matchAll(/^=== \[gen8metronomebattle\] ([^\n]*?) ===\n(.+?)\n\n\n/gms)]
     .reduce((dict, match) => {
         const team = Teams.import(match[2]);
@@ -37,60 +48,68 @@ const teams = [...file
         if (m) {
             name = m[2];
         }
-        if (allowIllegalTeams || validator.validateTeam(team) === null) {
-            dict[name] = Teams.pack(team)
-        } else {
-            console.log(`${name}: illegal team for metronome battle`)
+        dict[name] = Teams.pack(team)
+
+        // Warn if team is illegal
+        const issues = validator.validateTeam(team)
+        if (issues !== null) {
+            console.log(`Warning: team "${name}" is illegal for metronome battle:\n${issues}`)
         }
         return dict
     }, {})
+}
+const oppTeams = loadTeams(opponentsFile)
+const challTeams = (isChallengerMode ? loadTeams(challengersFile) : oppTeams)
 
-const teamNames = Object.keys(teams);
+// Simulation preamble and setup
+const oppNames = Object.keys(oppTeams);
+const challNames = Object.keys(challTeams);
 const promises = [];
-let winMatrix = Array(teamNames.length).fill().map(()=>Array(teamNames.length).fill(0));
-let totalTrials;
+let winMatrix = Array(oppNames.length).fill().map(()=>Array(challNames.length).fill(0));
+const totalTrials = trials * oppNames.length * (isChallengerMode ? challNames.length : (challNames.length - 1) / 2)
 
-if (challengerMode) {
-    console.log(`Beginning simulation of one team challenging all ${teamNames.length - 1} opponents ${trials} times each`);
-    console.log(`Challenger:\t${teamNames[0]}`)
-    console.log(`Teams:`);
-    teamNames.slice(1).forEach((x, i) => console.log(`  ${i+1}\t${x}`));
-    totalTrials = trials * (teamNames.length - 1);
+if (isChallengerMode) {
+    console.log(`${challNames.length} challengers each facing off against all ${oppNames.length} opponents ${trials} times`);
+    console.log(`Challengers:`)
+    challNames.forEach((x, i) => console.log(`  ${i+1}\t${x}`));
+    console.log(`Opponents:`);
+    oppNames.forEach((x, i) => console.log(`  ${i+1}\t${x}`));
 } else {
-    console.log(`Beginning tournament of ${teamNames.length} teams with each pair facing off ${trials} times`);
+    console.log(`Round robin of ${oppNames.length} teams with each pair facing off ${trials} times`);
     console.log(`Teams:`);
-    teamNames.forEach((x, i) => console.log(`  ${i+1}\t${x}`));
-    totalTrials = trials * (teamNames.length * (teamNames.length - 1) / 2);
+    oppNames.forEach((x, i) => console.log(`  ${i+1}\t${x}`));
 }
 
+// Progress bar
 const pbar = new cliProgress.SingleBar({format: '[{bar}] {percentage}% | Time: {duration_formatted} | ETA: {eta_formatted} | {value}/{total}'});
 pbar.start(totalTrials, 0);
 
+// Worker pool for multithreading
 const pool = workerpool.pool(__dirname + '/simWorker.js');
 
+// The simulation dispatcher (see the actual battle code in simWorker.js)
 promises.push((async () => {
     for (let trial = 0; trial < trials; trial++) {
-        const promisesRound = []; 
-
-        const maxI = challengerMode ? 0 : teamNames.length - 1;
-        for (let i = 0; i <= maxI; i++) {
-            for (let j = i + 1; j < teamNames.length; j++) {
-                let promise = pool.exec('simBattle', [teams[teamNames[i]], teams[teamNames[j]]])
+        for (let i = 0; i < oppNames.length; i++) {
+            let opponent = oppTeams[oppNames[i]]
+            for (let j = (isChallengerMode ? 0 : i + 1); j < challNames.length; j++) {
+                let challenger = challTeams[challNames[j]]
+                let promise = pool.exec('simBattle', [challenger, opponent])
                 promise.then((winner) => {
                     if (winner == "P1") {
-                        winMatrix[j][i]++
-                    } else {
                         winMatrix[i][j]++
+                    } else if (!isChallengerMode) {
+                        winMatrix[j][i]++
                     }
                     pbar.increment();
                 });
                 promises.push(promise)
-                promisesRound.push(promise)
             }
         }
     }
 })());
 
+// Function to colorize numbers from red to green
 function getColor(n, min, max) {
     if (n < min || n > max) {
         return chalk.yellow;
@@ -108,6 +127,7 @@ function getColor(n, min, max) {
     }
 }
 
+// Function for pretty-printing matrices
 // Modified from https://gist.github.com/lbn/3d6963731261f76330af
 function matprint(mat, min, max) {
     let shape = [mat.length, mat[0].length];
@@ -129,46 +149,38 @@ function matprint(mat, min, max) {
     });
 }
 
+// After all sims are done, calculate and display results
 Promise.all(promises).then(() => {
     pbar.stop();
 
     winMatrix = winMatrix.map((row) => row.map((x) => (x / trials * 100)))
 
-    const strWinMatrix = winMatrix.map((row) => row.map((x) => x.toFixed(1)))
-    for (let i = 0; i < teamNames.length; i++) {
-        strWinMatrix[i][i] = "-"
-        strWinMatrix[i].unshift(i + (challengerMode ? 0 : 1)) // Add labels
+    let minVal = Math.max(2 * Math.min(...winMatrix.map((row, i) => row.filter((_, j) => j != i)).flat()) - 50, 0);
+    let maxVal = Math.min(2 * Math.max(...winMatrix.map((row, i) => row.filter((_, j) => j != i)).flat()) - 50, 100);
+
+    const strWinMatrix = winMatrix.map((row) => row.map((x) => x.toFixed(1)))    
+    
+    // Blank out diagonals in round robin
+    if (!isChallengerMode) {
+        strWinMatrix.forEach((row, i) => {
+            if (i < row.length) {
+                row[i] = "-"
+            }
+        })
     }
-    strWinMatrix.unshift(Array.from(Array(teamNames.length + 1).keys())) // Add labels
+    
+    // Add labels
+    strWinMatrix.forEach((row, i) => row.unshift(i + 1))
+    strWinMatrix.unshift(Array.from(Array(challNames.length + 1).keys()))
     strWinMatrix[0][0] = "" // Fix corner
 
-    if (challengerMode) {
-        let minVal = Math.min(
-            Math.min(...strWinMatrix.slice(2).map(row => row[1])),
-            Math.min(...strWinMatrix[1].slice(2))
-        )
-        let maxVal = Math.max(
-            Math.max(...strWinMatrix.slice(2).map(row => row[1])),
-            Math.max(...strWinMatrix[1].slice(2))
-        )
+    console.log("Winrate for challenger (column) vs opponent (row):")
+    console.log(`E.g. top right cell is how often challenger #${challNames.length} beats opponent #1`)
+    matprint(strWinMatrix, minVal, maxVal);
 
-        console.log("Winrate for challenger:")
-        let cutMat = strWinMatrix.slice(2).map((row, i) => row.slice(0, 2).concat(teamNames[i+1]));
-        cutMat.unshift(["", "", ""]) // Dummy label
-        matprint(cutMat, minVal, maxVal);
-    } else {
-        let minVal = Math.max(2 * Math.min(...winMatrix.map((row, i) => row.filter((_, j) => j != i)).flat()) - 50, 0);
-        let maxVal = Math.min(2 * Math.max(...winMatrix.map((row, i) => row.filter((_, j) => j != i)).flat()) - 50, 100);
-
-        console.log("Winrate of player (in row) vs opponent (in column)")
-        console.log(`E.g. top right cell is winrate of #1 vs #${teamNames.length}`)
-        matprint(strWinMatrix, minVal, maxVal);
-
-        console.log("\n\nOverall winrates:\n")
-
-        const avgRates = teamNames.map((name, i) => [name, winMatrix.reduce((sum, row, j) => i == j ? sum : sum + row[i], 0) / (winMatrix.length - 1)]).sort((a, b) => b[1]- a[1])
-        avgRates.forEach((pair, i) => console.log("   #" + (i+1) + "\t| " + getColor(pair[1], avgRates[avgRates.length-1][1], avgRates[0][1])(pair[1].toFixed(1)) + " | " + pair[0]))
-    }
+    console.log("\n\nOverall winrates:\n")
+    const avgRates = challNames.map((name, i) => [name, winMatrix.reduce((sum, row, j) => i == j ? sum : sum + row[i], 0) / (winMatrix.length - 1)]).sort((a, b) => b[1]- a[1])
+    avgRates.forEach((pair, i) => console.log("   #" + (i+1) + "\t| " + getColor(pair[1], avgRates[avgRates.length-1][1], avgRates[0][1])(pair[1].toFixed(1)) + " | " + pair[0]))
 
     pool.terminate();
 });
