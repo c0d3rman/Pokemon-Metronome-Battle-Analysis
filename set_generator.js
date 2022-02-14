822508f710af7acea6b0bb81e9dd3b7ac31f0773
MC1600594 <58523614+MC1600594@users.noreply.github.com>
2022-02-14 23:18:22 +0000

    lots of abilities added
    special cases and exceptions not done

--- set_generator.js
+++ set_generator.js
@@ -2,7 +2,7 @@ const {Dex, Teams} = require("./pokemon-showdown");
 const fs = require('fs')
 
 
-let species = 'Type: Null';
+let species = 'Cresselia';
 
 let items = [
   'Weakness Policy',
@@ -15,39 +15,116 @@ let items = [
   'Lum Berry',
   'Razor Claw',
   'Safety Goggles',
-  'Life Orb'
+  'Life Orb',
+  'Wide Lens',
+  'Rowap Berry',
+  'Jaboca Berry',
+  'Kings Rock'
 ]
 
 let abilities = [
-  'Intrepid Sword',
-  'Dauntless Shield',
-  'Fur Coat',
-  'Ice Scales',
-  'Magic Bounce',
-  'Magic Guard',
-  'Competitive',
+  //
+  //S-Tier 
+  //
+  'Comatose',
+  'Competitive', // TODO: only if SpA > Atk
   'Defiant',
   'Flower Veil',
+  'Intrepid Sword',
+  'Magic Bounce',
+  'Magic Guard',
+  'Mold Breaker',
+  'Prism Armor',
+  'Tinted Lens',
+  'Unaware',
+  'Water Bubble', // TODO: only if water type or weak to fire
+  
+  //
+  //A-Tier
+  //
+  'Aerilate', // TODO: only if flying type
+  'As One(Glastrier)',
+  'As One(Spectrier)', // TODO: only if SpA > Atk
+  'Analytic', // TODO: probably only when slow and teammate is faster
+  'Dauntless Shield',
+  'Delta Stream', // TODO: only if mon is flying type
+  'Desolate Land', // TODO: only if team is weak to water
+  'Download', // TODO: only if SpA > Atk
+  'Flash Fire', // TODO: only if mon is weak to fire
   'Friend Guard',
-  'Simple',
-  'Imposter',
-  'Compound Eyes',
-  'Desolate Land',
-  'Primordial Sea',
-  'Download',
+  'Ice Scales',
+  'Imposter', // TODO: only leppa blissey or light ball pikachu or thick club marowak/marowak-a
+  'Lightning Rod', // TODO: only if mon is weak to electric, or SpA > Atk
   'Mirror Armor',
-  'Innards Out',
-  'Storm Drain',
-  'Lightning Rod',
-  'Analytic',
-  'Plus',
-  'Prism Armor'
+  'Misty Surge',
+  'Mummy',
+  'Pixilate', // TODO: only if mon is a fairy
+  'Plus', // TODO: both mons, and only if SpA > Atk
+  'Primordial Sea', // TODO: only if mon is weak to fire or a water type
+  'Refrigerate', // TODO: only if mon is ice type
+  'Serene Grace',
+  'Sheer Force',
+  'Simple',
+  'Storm Drain', // TODO: only if mon is weak to water, or SpA > Atk
+  'Thick Fat',
+  'Trace',
+  
+  //
+  //B-Tier and lower
+  //
+  'Air Lock',
+  'Bulletproof',
+  'Compound Eyes',
+  'Cute Charm',
+  'Contrary',
+  'Dancer',
+  'Effect Spore',
+  'Electric Surge', // TODO: only if the mon is electric type
+  'Flame Body',
+  'Full Metal Body',
+  'Galvanize', // TODO: only if mon is electric type
+  'Harvest', // TODO: only if mon is holding a berry
+  'Healer', // TODO: not a grass type, they have veil
+  'Hydration', // TODO: only if teammate has Primordial Sea
+  'Illusion', // TODO: Slot 1 only
+  'Innards Out', // TODO: the shit chansey set only
+  'Infiltrator',
+  'Intimidate',
+  'Levitate', // TODO: only if mon is weak to ground
+  'Liquid Voice', // TODO: only if teammate has storm drain, yes this works for perish song btw
+  'Long Reach',
+  'Magician', // TODO: only if WP or no item
+  'Neuroforce',
+  'No Guard',
+  'Normalize', // TODO: only if mon is normal type
+  'Overcoat', // TODO: only if the mon isnt grass type
+  'Pastel Veil', // TODO: only if mon is NOT poison type
+  'Pickup', // TODO: only if WP, Rowap or Jaboca on both mons
+  'Poison Point',
+  'Poison Touch',
+  'Power Spot',
+  'Punk Rock',
+  'Sap Sipper', // TODO: only if mon is weak to grass
+  'Scrappy', // TODO: only if mon is normal or fighting type
+  'Shadow Shield',
+  'Shed Skin', // TODO: only if the mon isnt grass type
+  'Shield Dust', // TODO: only if the mon isnt grass type
+  'Sniper',
+  'Soul Heart', // TODO: only if SpA > Atk
+  'Soundproof',
+  'Steely Spirit', // TODO: only if mon is steel type
+  'Stench', // TODO: only if mon is NOT holding a Kings Rock
+  'Super Luck',
+  'Synchronize',
+  'Truant', //one way to win the PP war, I suppose
+  'Wonder Skin'
 ]
 
 let natures = [
   'Brave',
   'Quiet',
-  'Relaxed'
+  'Relaxed',
+  'Sassy'
 ]
 
 let speeds = [
