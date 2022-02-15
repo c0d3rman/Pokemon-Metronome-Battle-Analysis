60b9dcdf6d11e2d76f05d6eee068959e93e65ba2
Yoni Lerner <yonilern@gmail.com>
2022-02-15 06:16:53 -0800

    Overhaul of meta explorer

--- meta_explorer.js
+++ meta_explorer.js
@@ -4,109 +4,108 @@ const cliProgress = require('cli-progress');
 const workerpool = require('workerpool');
 
 
-const obj = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
+let obj = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
 
 
-function inc(dict, key, amount=1) {
-	if (!(key in dict)) {
-		dict[key] = 0;
-	}
-	dict[key] += amount;
-}
+// class Beta extends Array {
+//     constructor() {
+// 		super(1, 1)
+// 	}
 
-for (const name in obj.data) {
-	if (name == "_total") {
-		continue
-	}
+// 	getP() {
+// 		return this[0] / (this[0] + this[1])
+// 	}
 
-	// Process spreads into natures and speeds
-	obj.data[name]['Natures'] = {}
-	obj.data[name]['Speeds'] = {}
-	Object.keys(obj.data[name]['Spreads']).forEach(spread => {
-		const [nature, speed] = spread.match(/^(\w+):.+\/(\d+)$/).slice(1, 3);
-		inc(obj.data[name]['Natures'], nature, obj.data[name]['Spreads'][spread]);
-		inc(obj.data[name]['Speeds'], speed, obj.data[name]['Spreads'][spread]);
-	})
+// 	toString() {
+// 		return super.toString() + " (" + this.getP() + ")"
+// 	}
+// }
 
-	// Delete weirdness (and 'empty') from teammates
-	for (const teammate in obj.data[name].Teammates) {
-		if (!(teammate in obj.data)) {
-			delete obj.data[name].Teammates[teammate]
-		}
-	}
+function getP(l) {
+	return l[0] / (l[0] + l[1])
 }
 
 
-// Modified from https://gist.github.com/lbn/3d6963731261f76330af
-function matprint(mat) {
-    let shape = [mat.length, mat[0].length];
-    function col(mat, i) {
-        return mat.map(row => row[i]);
-    }
-    let colMaxes = [];
-    for (let i = 0; i < shape[1]; i++) {
-        colMaxes.push(Math.max.apply(null, col(mat, i).map(n => n.toString().length)));
-    }
+// If the input is a raw data file, pre-process it into the dict containing just the stuff we need
+if ("cutoff" in obj.info) {
+	obj = {
+		info: {sim_battles: 0},
+		data: Object.keys(obj.data).reduce((dict, name) => {
+			dict[name] = {
+				winrate: [1, 1],
+				Speeds: {"0": [1, 1], "252": [1, 1]},
+				Abilities: Object.keys(obj.data[name].Abilities)
+					.reduce((d, k) => { d[Dex.abilities.get(k).name] = [1, 1]; return d }, {}),
+				Items: Object.keys(obj.data[name].Items)
+					.reduce((d, k) => { d[Dex.items.get(k).name] = [1, 1]; return d }, {}),
+				Natures: Object.keys(obj.data[name].Spreads)
+					.reduce((d, k) => { d[k.match(/^(\w+):/)[1]] = [1, 1]; return d }, {})
+				// TBD Teammates
+			}
 
-    mat.forEach(row => {
-        console.log.apply(null, row.map((val, j) => {
-            return "  " + val.toString() + new Array(colMaxes[j]-val.toString().length+1).join(" ");
-        }));
-    });
+			return dict
+		}, {})
+	}
 }
 
-
+// Choose a random key from a dict weighted by its values
 function chooseRand(dict, opts={}) {
 	let z = 'key' in opts
 
-	if ('defaults' in opts) {
-		for (const def of opts.defaults) {
-			if (!(def in dict) && def != "_total") {
-				z ? dict[def][opts.key] = 1 : dict[def] = 1
-			}
+	// if ('defaults' in opts) {
+	// 	for (const def of opts.defaults) {
+	// 		if (!(def in dict) && def != "_total") {
+	// 			z ? dict[def][opts.key] = 1 : dict[def] = 1
+	// 		}
+	// 	}
+	// }
+
+
+	// return Object.keys(dict)[Math.floor(Math.random() * Object.keys(dict).length)]
+	let total = Object.keys(dict).reduce((sum, k) => sum + getP(z ? dict[k][opts.key] : dict[k]), 0)
+	let x = Math.random() * total;
+	let runningSum = 0;
+	let keys = Object.keys(dict);
+	for (const k of keys) {
+		runningSum += getP(z ? dict[k][opts['key']] : dict[k]);
+		if (runningSum >= x) {
+			return k;
 		}
 	}
-
-	if (!('_total' in dict)) {
-		dict["_total"] = Object.keys(dict).reduce((sum, k) => sum + (z ? dict[k][opts.key] : dict[k]), 0)
-	}
-
-	let x = Math.random() * dict["_total"];
-	return Object.keys(dict)
-		.reduce((tot, k) => {
-			if (typeof tot != 'number') {
-				return tot;
-			}
-			let newTot = tot + (z ? dict[k][opts.key] : dict[k]);
-			return newTot >= x ? k : newTot
-		}, 0)
+	return keys[keys.length - 1]
 }
 
+// Choose a random set for a given pokemon
 function chooseSet(name) {
 	let minSpeed = chooseRand(obj.data[name].Speeds, {defaults: ["0", "252"]}) == "0";
 	return {
           species: name,
-          item: Dex.items.get(chooseRand(obj.data[name].Items)).name,
-          ability: Dex.abilities.get(chooseRand(obj.data[name].Abilities)).name,
+          item: chooseRand(obj.data[name].Items),
+          ability: chooseRand(obj.data[name].Abilities),
           nature: chooseRand(obj.data[name].Natures, {defaults: ["Brave", "Quiet", "Relaxed"]}),
-          evs: { hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: (minSpeed ? 0 : 255) },
+          evs: { hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: (minSpeed ? 0 : 252) },
           ivs: { hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: (minSpeed ? 0 : 31) },
           moves: ['Metronome']
         }
 }
 
+// Choose a random team of 2 pokemon
 function chooseTeam() {
-	const mon1 = chooseRand(obj.data, {key: 'Raw count'})
+	const mon1 = chooseRand(obj.data, {key: 'winrate'})
 	const set1 = chooseSet(mon1)
-	const mon2 = chooseRand(obj.data[mon1].Teammates, {defaults: Object.keys(obj.data)})
+	// const mon2 = chooseRand(obj.data[mon1].Teammates, {defaults: Object.keys(obj.data)})
+	const mon2 = chooseRand(obj.data)
 	const set2 = chooseSet(mon2)
 	return [set1, set2]
 }
+
 
 const trialsPerBatch = 10000;
 
 // Worker pool for multithreading
 const pool = workerpool.pool(__dirname + '/simWorker.js');
+
+let first = true;
 
 (async () => {
 	while (true) {
@@ -125,17 +124,17 @@ const pool = workerpool.pool(__dirname + '/simWorker.js');
 	    			promise.then((winner) => {
 							const teamW = (winner == "P1" ? team1 : team2)
 							for (const team of [team1, team2]) {
-								const n = team === teamW ? 1 : -1;
+								const i = team === teamW ? 0 : 1;
 								for (const [set, teammate] of [team, [team[1], team[0]]]) {
-									inc(obj.data[set.species], 'Raw count', n);
-									inc(obj.data[set.species]['Items'], set.item, n);
-									inc(obj.data[set.species]['Abilities'], set.ability, n);
-									inc(obj.data[set.species]['Natures'], set.nature, n);
-									inc(obj.data[set.species]['Speeds'], set.evs.spe == 0 ? "0" : "252", n);
-									inc(obj.data[set.species]['Teammates'], teammate.species, n);
+									obj.data[set.species].winrate[i]++
+									obj.data[set.species].Items[set.item][i]++
+									obj.data[set.species].Abilities[set.ability][i]++
+									obj.data[set.species].Natures[set.nature][i]++
+									obj.data[set.species].Speeds[set.evs.spe == 0 ? "0" : "252"][i]++
+									// obj.data[set.species].Teammates[teammate.species][i]++
 								}
 							}
-							inc(obj.info, "sim_battles");
+							obj.info["sim_battles"]++
 							pbar.increment();
 	        		});
 		       	 	promises.push(promise)
@@ -159,9 +158,28 @@ const pool = workerpool.pool(__dirname + '/simWorker.js');
 // }
 
 
+// Modified from https://gist.github.com/lbn/3d6963731261f76330af
+// function matprint(mat) {
+//     let shape = [mat.length, mat[0].length];
+//     function col(mat, i) {
+//         return mat.map(row => row[i]);
+//     }
+//     let colMaxes = [];
+//     for (let i = 0; i < shape[1]; i++) {
+//         colMaxes.push(Math.max.apply(null, col(mat, i).map(n => n.toString().length)));
+//     }
+
+//     mat.forEach(row => {
+//         console.log.apply(null, row.map((val, j) => {
+//             return "  " + val.toString() + new Array(colMaxes[j]-val.toString().length+1).join(" ");
+//         }));
+//     });
+// }
 
 // matprint(Object.keys(names)
 // 	.sort((a, b) => names[b] - names[a])
 // 	.map((n, i) => ["#" + (i+1), n, (names[n]/trials).toFixed(5), (obj.data[n]['Raw count']/obj.data._total).toFixed(5)])
 // )
+
+
 
