1b0a5a97ead245ee455356d3ddda7c2a294c7073
Yoni Lerner <yonilern@gmail.com>
2022-02-16 23:49:31 -0800

    Made meta explorer work with actual meta data

--- meta_explorer.js
+++ meta_explorer.js
@@ -4,8 +4,7 @@ const cliProgress = require('cli-progress');
 const workerpool = require('workerpool');
 const setGen = require("./set_generator");
 
-// Import data about meta to generate enemy teams
-// const metaObj = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
+
 
 class Beta extends Array {
 	constructor() {
@@ -22,6 +21,41 @@ class Beta extends Array {
 }
 
 
+
+
+// Import data about meta to generate enemy teams
+const rawMetaObj = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
+
+// Process raw meta file
+// if ('cutoff' in rawMetaObj.info) {
+const metaObj = {data: new Map()}
+
+for (const name in rawMetaObj.data) {
+	// Process spreads into natures and speeds
+	const natures = new Map()
+	const speeds = new Map([["0", 0], ["252", 0]])
+	Object.keys(rawMetaObj.data[name]['Spreads']).forEach(spread => {
+		let [nature, speed] = spread.match(/^(\w+):.+\/(\d+)$/).slice(1, 3);
+		speed = speed == "0" ? "0" : "252";
+		natures.set(nature, (natures.get(nature) || 0) + rawMetaObj.data[name]['Spreads'][spread]);
+		speeds.set(speed, (speeds.get(speed) || 0) + rawMetaObj.data[name]['Spreads'][spread]);
+	})
+
+	metaObj.data.set(Dex.species.get(name).name, new Map([
+		["count", rawMetaObj.data[name]['Raw count']],
+		["Speeds", speeds],
+		["Natures", natures],
+		["Abilities", new Map(Object.entries(rawMetaObj.data[name]['Abilities']).map(l => [Dex.abilities.get(l[0]).name, l[1]]))],
+		["Items", new Map(Object.entries(rawMetaObj.data[name]['Items']).map(l => [Dex.items.get(l[0]).name, l[1]]))],
+		// For teammates, delete weirdness (and 'empty'), and do a name correction to fix things like "Sirfetch'd" => "Sirfetch’d"
+		["Teammates", new Map(Object.entries(rawMetaObj.data[name]['Teammates'])
+			.map(l => [Dex.species.get(l[0]).name, l[1]])
+			.filter(l => l[0] in rawMetaObj.data))] 
+	]))
+}
+// }
+
+
 // Create output object
 const legalMons = Dex.species.all().filter(s => !s.types.includes('Steel') && s.bst <= 625 && s.name != "Pokestar Spirit").map(m => m.name).sort()
 let obj = {
@@ -33,7 +67,7 @@ let obj = {
 		["Items", setGen.items.reduce((m, k) => m.set(k, new Beta()), new Map())],
 		["Natures", setGen.natures.reduce((m, k) => m.set(k, new Beta()), new Map())],
 		["Teammates", legalMons.reduce((m, k) => m.set(k, new Beta()), new Map())]
-	])), new Map())
+		])), new Map())
 }
 
 // Modified from https://gist.github.com/lbn/3d6963731261f76330af
@@ -107,32 +141,29 @@ function chooseRand(map, accessor) {
 }
 
 // Choose a random set for a given pokemon
-function chooseSet(name) {
-	const speed = chooseRand(obj.data.get(name).get("Speeds"), v => v.getP());
-	// speed = "0";
+function chooseSet(name, obj) {
+	const speed = chooseRand(obj.data.get(name).get("Speeds"), v => v instanceof Beta ? v.getP() : v);
 	return {
 		species: name,
-		item: chooseRand(obj.data.get(name).get("Items"), v => v.getP()),
-		ability: chooseRand(obj.data.get(name).get("Abilities"), v => v.getP()),
-		nature: chooseRand(obj.data.get(name).get("Natures"), v => v.getP()),
-		// item: "Bright Powder", ability: "Intrepid Sword", nature: "Brave",
+		item: chooseRand(obj.data.get(name).get("Items"), v => v instanceof Beta ? v.getP() : v),
+		ability: chooseRand(obj.data.get(name).get("Abilities"), v => v instanceof Beta ? v.getP() : v),
+		nature: chooseRand(obj.data.get(name).get("Natures"), v => v instanceof Beta ? v.getP() : v),
 		evs: { hp: 252, atk: 252, def: 252, spa: 252, spd: 252, spe: (speed == "0" ? 0 : 252) },
 		ivs: { hp: 31, atk: 31, def: 31, spa: 31, spd: 31, spe: (speed == "0" ? 0 : 31) },
 		moves: ['Metronome']
 	}
 }
 
-let tempSet = chooseSet("Bulbasaur")
 
 // Choose a random team of 2 pokemon
-function chooseTeam() {
-	const mon1 = chooseRand(obj.data, v => v.get('winrate').getP())
-	const set1 = chooseSet(mon1)
-	const mon2 = chooseRand(obj.data.get(mon1).get("Teammates"), v => v.getP())
-	// const mon2 = chooseRand(obj.data, v => v['winrate'].getP())
-	const set2 = chooseSet(mon2)
+function chooseTeam(obj) {
+	const mon1 = chooseRand(obj.data, v => v.has('winrate') ? v.get('winrate').getP() : v.get('count'))
+	const set1 = chooseSet(mon1, obj)
+	let teammates = obj.data.get(mon1).get("Teammates");
+	if (teammates.size == 0) teammates = new Map([...obj.data.keys()].map(k => [k, 1]))
+	const mon2 = chooseRand(teammates, v => v instanceof Beta ? v.getP() : v)
+	const set2 = chooseSet(mon2, obj)
 	return [set1, set2]
-	// return [tempSet, tempSet]
 }
 
 
@@ -145,6 +176,7 @@ const pool = workerpool.pool(__dirname + '/simWorker.js');
 
 let counter = 0;
 
+
 (async () => {
 	while (true) {
 		// Progress bar
@@ -156,34 +188,32 @@ let counter = 0;
 		const toInc = []
 
 		// The simulation dispatcher (see the actual battle code in simWorker.js)
-		promises.push((async () => {
-			for (let trial = 0; trial < trialsPerBatch; trial++) {
-				const team1 = chooseTeam();
-				const team2 = chooseTeam();
-				const promise = pool.exec('simBattle', [Teams.pack(team1), Teams.pack(team2)])
-				promise.then((winner) => {
-					const teamW = (winner == "P1" ? team1 : team2)
-					for (const team of [team1, team2]) {
-						const i = team === teamW ? 0 : 1;
-						for (const [set, teammate] of [team, [team[1], team[0]]]) {
-							toInc.push([obj.data.get(set.species).get("winrate"), i])
-							toInc.push([obj.data.get(set.species).get("Items").get(set.item), i])
-							toInc.push([obj.data.get(set.species).get("Abilities").get(set.ability), i])
-							toInc.push([obj.data.get(set.species).get("Natures").get(set.nature), i])
-							toInc.push([obj.data.get(set.species).get("Speeds").get(set.evs.spe == 0 ? "0" : "252"), i])
-							// if (!(teammate.species in obj.data[set.species].Teammates)) {
-							// 	obj.data[set.species].Teammates[teammate.species] = new Beta()
-							// }
-							toInc.push([obj.data.get(set.species).get("Teammates").get(teammate.species), i])
-						}
+		for (let trial = 0; trial < trialsPerBatch; trial++) {
+			const team1 = chooseTeam(obj);
+			const team2 = chooseTeam(metaObj);
+			const promise = pool.exec('simBattle', [Teams.pack(team1), Teams.pack(team2)])
+			promise.then((winner) => {
+				const teamW = (winner == "P1" ? team1 : team2)
+				// for (const team of [team1, team2]) {
+				let team = team1
+				const i = team === teamW ? 0 : 1;
+				for (const [set, teammate] of [team, [team[1], team[0]]]) {
+					if (!obj.data.get(set.species).get("winrate")) {
+						console.log(set.species)
 					}
-					obj.info["sim_battles"]++
-					pbar.increment();
-				});
-				// pbar.increment();
-				promises.push(promise)
-			}
-		})());
+					toInc.push([obj.data.get(set.species).get("winrate"), i])
+					toInc.push([obj.data.get(set.species).get("Items").get(set.item), i])
+					toInc.push([obj.data.get(set.species).get("Abilities").get(set.ability), i])
+					toInc.push([obj.data.get(set.species).get("Natures").get(set.nature), i])
+					toInc.push([obj.data.get(set.species).get("Speeds").get(set.evs.spe == 0 ? "0" : "252"), i])
+					toInc.push([obj.data.get(set.species).get("Teammates").get(teammate.species), i])
+				}
+				// }
+				obj.info["sim_battles"]++
+				pbar.increment();
+			});
+			promises.push(promise)
+		}
 
 		await Promise.all(promises);
 
@@ -200,7 +230,7 @@ let counter = 0;
 			.sort((a, b) => b[1] - a[1])
 			.slice(0, 20)
 			.map((l, i) => ["#" + (i + 1), l[0], l[1].toFixed(5)])
-		)
+			)
 	}
 })();
 
