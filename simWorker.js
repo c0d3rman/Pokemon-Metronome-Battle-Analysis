0c3d9525046bb48ddcb8a234428e2d5f4529346c
Yoni Lerner <yonilern@gmail.com>
2022-02-24 00:35:26 -0800

    Added dynamax logic and streamlined simulation
    Also now actually errors when the battle breaks

--- simWorker.js
+++ simWorker.js
@@ -1,41 +1,54 @@
 const workerpool = require('workerpool');
 const {BattleStream} = require('./pokemon-showdown');
 
+function chooseMove(stream, mySide) {
+    const side = stream.battle.sides[mySide]
+    const allies = side.allies()
+
+    let doDyna = 0;
+    if (side.canDynamaxNow() && side.baseMoveSlots) {
+        if (allies.length == 1) {
+            if (allies[0].moveSlots[0].id == "metronome" && allies[0].baseMoveSlots[0].pp == 1) {
+                doDyna = 1
+            }
+        } else {
+            if (allies[0].moveSlots[0].id == "metronome" && allies[0].baseMoveSlots[0].pp == 1 && allies[1].baseMoveSlots[0].pp <= 1) {
+                doDyna = 1;
+            } else if (allies[1].moveSlots[0].id == "metronome" && allies[0].baseMoveSlots[0].pp == 0 && allies[1].baseMoveSlots[0].pp == 1) {
+                doDyna = 2;
+            }
+        }
+    }
+    
+    let out = `>p${mySide+1} move 1`;
+    if (doDyna == 1) {
+        out += " dynamax"
+    }
+    if (allies.length == 2) {
+        out += ", move 1"
+        if (doDyna == 2) {
+            out += " dynamax"
+        }
+    }
+    return out
+}
+
 async function simBattle(team1, team2) {
     const stream = new BattleStream();
 
-    stream.write(`>start {"formatid":"gen8metronomebattle"}`);
+    stream.write(`>start {"formatid":"gen8metronomebattle","strictChoices":true}`);
     stream.write(`>player p1 {"name":"P1","team":"${team1}"}`);
     stream.write(`>player p2 {"name":"P2","team":"${team2}"}`);
 
-    let p1alive = 2;
-    let p2alive = 2;
-
     for await (const output of stream) {
-        for (const match of output.matchAll(/\|faint\|p([12])[ab]: /g)) {
-            if (match[1] == '1') {
-                p1alive--;
-            } else {
-                p2alive--;
-            }
-        }
-
         const m = output.match(/\|win\|(P[12])/);
         if (m) {
             return m[1];
         }
 
         if (/\|turn\|\d+$/.test(output)) {
-            if (p1alive == 2) {
-                stream.write(`>p1 move 1, move 1`);
-            } else {
-                stream.write(`>p1 move 1`);
-            }
-            if (p2alive == 2) {
-                stream.write(`>p2 move 1, move 1`);
-            } else {
-                stream.write(`>p2 move 1`);
-            }
+            stream.write(chooseMove(stream, 0))
+            stream.write(chooseMove(stream, 1))
         }
     }
 }
