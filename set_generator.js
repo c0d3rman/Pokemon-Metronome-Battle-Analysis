9a1621e2bb344019688ac254eec4e8095f62f22e
MC1600594 <58523614+MC1600594@users.noreply.github.com>
2022-02-24 05:57:58 +0000

    actual scorched earth

--- set_generator.js
+++ set_generator.js
@@ -30,10 +30,8 @@ let items = [
   'Weakness Policy', // +2 SpA + Atk when hit SE
   'Choice Band', // x1.5 Atk
   'Choice Specs', // x1.5 SpA
-  'Kee Berry', // +1 Def
   'Eviolite', // x1.5 Def + SpD if NFE
   'Leppa Berry', // +5 PP
-  'Bright Powder', // Opponents 0.9x Acc
   'Lum Berry', // Cure any status
   'Life Orb', // x1.3 Dmg, -10% HP per attack
 ]
@@ -44,7 +42,6 @@ let abilities = [
   //
   'Comatose', // immune to status, considered asleep
   'Competitive', // +2 SpA on stat drop
-  'Defiant', // +2 Atk on stat drop
   'Flower Veil', // grass types are immune to stat drops and status
   'Intrepid Sword', // +1 Atk on switch-in
   'Magic Bounce', // most status moves "bounce" off and hit user
@@ -60,7 +57,6 @@ let abilities = [
   'Aerilate', // normal moves are now flying type and 1.2x power
   'As One(Glastrier)', // opponent cannot use berries, +1 Atk on KO
   'As One(Spectrier)', // opponent cannot use berries, +1 SpA on KO
-  'Dauntless Shield', // +1 Def on switch-in
   'Delta Stream', // overrides all weather, flying type weaknesses are ignored
   'Desolate Land', // permanent sun, water attacks fail
   'Flash Fire', // immunity to fire, fire moves 1.5x power when hit by a fire move
@@ -85,15 +81,10 @@ let abilities = [
   'Intimidate', // -1 Opponents Atk on switch-in
   'Levitate', // immune to ground moves
   'Normalize', // all moves are normal type, 1.2x power if type was changed
-  'Overcoat', // immune to weather damage and powder moves
-  'Poison Point', // 30% chance of poisoning enemy when hit by a contact move
   'Poison Touch', // 30% chance of poisoning enemy on contact
   'Power Spot', // ally has 1.3x power
   'Sap Sipper', // immune to grass, +1 Atk when hit by a grass attack
   'Scrappy', // can hit ghosts with normal and fighting type attacks
-  'Shed Skin', // 33% chance of curing status every turn
-  'Shield Dust', // immune to secondary effects
-  'Soul Heart', // when a mon faints, +1 SpA
   'Synchronize', // when statused, opponent is too
 ]
 
